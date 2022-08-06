661c6ab9aa8dc93e400538b130f892ab55edab18
Peter Gamali <petrugamali@gmail.com>
2022-08-06 22:51:05 +0300

    Fixed a validation bug

--- src/Components/Textinput/Textinput.jsx
+++ src/Components/Textinput/Textinput.jsx
@@ -1,30 +1,36 @@
-import React, { useState, useReducer } from 'react'
-import { useValidation } from '../../Hooks/useValidation'
+import React, { useState, useEffect } from 'react'
+import { validate } from '../../Functions/Validation.js'
 
 
 export default function Textinput({ inputClass, id, onChange, labelText, validations }) {
 
     const [value, setValue] = useState('')
     const [validationErrors, setValidationErrors] = useState([])
+    const [isDirty, setIsDirty] = useState(false)
     
-    const errors = useValidation(value, validations)
-
     const handleOnChange = (e) => {
         setValue(() => e.target.value)
         onChange(e)
-        if (errors) setValidationErrors(() => errors)
+    }
+    
+    const handleOnBlur = () => {
+        setIsDirty(true)
     }
 
-    const handleOnBlur = () => {
-        if (errors) setValidationErrors(() => errors)
-    }
+    useEffect(() => {
+        if (isDirty) {
+            const errors = validate(value, validations)
+            if (errors) setValidationErrors(() => errors)
+        }
+    }, [value])
 
     return (
         <div className={inputClass}>
             {labelText !== undefined && <label htmlFor={id}>{labelText}</label>}
             <input type="text" placeholder={labelText} value={value} id={id} onChange={(e) => handleOnChange(e)} onBlur={handleOnBlur} />
             {validationErrors !== [] && validationErrors.map(item => {
-                if (Object.values(item)[0] === false) {return}
+                // eslint-disable-next-line
+                if (Object.values(item)[0] === false) { return }
                 return (<div className="error" key={Object.keys(item)[0]}>{item.msg}</div>)
             })}
         </div>
---
+++ src/Functions/Validation.js
@@ -0,0 +1,37 @@
+export const validate = (value, validations) => {
+    let isEmptyError = true
+    let minLengthError = false
+    let maxLengthError = false
+    let isEmailError = false
+
+    for (const validation in validations) {
+        switch (validation) {
+            case 'isNotEmpty':
+                if (value) {
+                    isEmptyError = false
+                } else { isEmptyError = true }
+                break;
+            case 'minLength':
+                if (value.length < validations[validation]) minLengthError = true
+                break;
+            case 'maxLength':
+                if (value.length > validations[validation]) maxLengthError = true
+                break;
+            case 'isEmail':
+                if (String(value)
+                    .toLowerCase().match(
+                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    )) isEmailError = true
+                break;
+            default:
+        }
+    }
+
+
+    return [
+        { isEmptyError, msg: "Field is empty" },
+        { minLengthError, msg: `Field is too short` },
+        { maxLengthError, msg: `Field has too many characters` },
+        { isEmailError, msg: "Field is not an email" }
+    ]
+}
