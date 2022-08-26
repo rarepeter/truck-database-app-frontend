6fb3ab964478d149859188444c005004c46aa638
Peter Gamali <petrugamali@gmail.com>
2022-08-26 22:41:20 +0300

    Added axios interceptor for responses. Now, if the access token has expired, the system will refresh it automatically if it detects the expiry and the refresh token still exists

--- src/Http/index.js
+++ src/Http/index.js
@@ -11,4 +11,22 @@ $api.interceptors.request.use((config) => {
     return config
 })
 
+$api.interceptors.response.use((config) => {
+    return config
+}, async (error) => {
+    const originalRequest = error.config
+    if (error.response.status == 401 && error.config && !error.config._isRetried) {
+        originalRequest._isRetried = true
+        try {
+            const response = await axios.get(`${serverURL}/refresh`, { withCredentials: true })
+            localStorage.setItem(`token`, response.data.accessToken)
+            return $api.request(originalRequest)
+
+        } catch (e) {
+            console.log("User not authorized")
+        }
+    }
+    throw error
+})
+
 export default $api
